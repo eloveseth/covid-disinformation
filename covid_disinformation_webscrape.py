9c13a36e53cb2996677f5a02edd925e0df04eed8
eloveseth <60454054+eloveseth@users.noreply.github.com>
2021-02-22 19:47:29 -0800

    Update covid_disinformation_webscrape.py

--- covid_disinformation_webscrape.py
+++ covid_disinformation_webscrape.py
@@ -1,11 +1,12 @@
+##Elle Loveseth
+##Purpose: 
+##  Scrape article content from articles identified as having covid disinformation.
+##  Append to existing training set of real and fake news.
+##Updated: 2/22/2021
+
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[123]:
-
-
-#!pip install beautifulsoup4
-#!pip install requests
 
 import pandas as pd
 import numpy as np
@@ -13,37 +14,33 @@ from bs4 import BeautifulSoup as beautsoup
 import requests as req
 import sklearn as sk
 
-
-# In[2]:
-
-
 #initial training set data source: https://www.kaggle.com/clmentbisaillon/fake-and-real-news-dataset
 #includes two general datasets containing "true" news articles and "fake" news articles
 #we'll use this as a baseline + add additional true and fake news articles scraped from the web to expand the dataset
 
+#import datasets and code with a binary indicator variable to identify fake articles
 training = pd.read_csv ('C://users/elove/Dropbox/Projects/Covid Disinformation/True.csv')
 training['fake'] = 0
 
 fake = pd.read_csv ('C://users/elove/Dropbox/Projects/Covid Disinformation/Fake.csv')
 fake['fake'] = 1
 
+#append datasets and code with binary indicator to identify covid-related articles
+#this dataset only contains pre-covid articles, so it is coded accordingly
 training = training.append(fake)
 training['covid'] = 0
 print(training)
 
-
-# In[155]:
-
-
-#scrape fake covid-19 articles using BeautifulSoup
-
+#we'll now scrape fake covid-19 articles using BeautifulSoup
+#first, we'll upload a table of links to fake articles and their source
 articles = pd.read_csv ('C://users/elove/Dropbox/Projects/Covid Disinformation/fake_covid.csv')
-#articles = articles[articles['source'].isin(['Great Game India', 'Infowars', 'Freedom Articles'])]
-#articles = articles.reset_index(drop = True)
+
+#create lists of each column to use in the for loop
 articles_list = list(articles['url'])
 source_list = list(articles['source'])
 
-#create a dictionary that contains the class code for each source
+#to scrape articles, we need to identify the appropriate class code within the website's html code
+#we inspect the website code source and track the class code in a dictionary corresponding with the article source
 class_dictionary = {'Great Game India' : 'td-post-content lazyload',
                     'Infowars' : 'text' ,
                     'Health Impact News' : 'post-content',
@@ -55,33 +52,36 @@ text_list = []
 time_list = []
 
 for num in np.arange(0, len(articles_list)) :
-        
+    
+    #identify the url by position
     url = articles_list[num]
+    
+    #scrape the title of the article and append to list of titles
     html = req.get(url).text
     soup = beautsoup(html, 'html.parser')
     title = soup.find('title').get_text()
     title_list.append(title)
         
-    #time = soup.find('time').get_text()
-    #time_list.append(time)
-        
+    #to scrape the content, we'll identify the class code using our dictionary and the article source
     source = source_list[num]
     class_key = class_dictionary[source]
 
+    #scrape all content paragraphs from the article
     article = req.get(url)
     content = article.content
     soup = beautsoup(content, 'html5lib')
-    body = soup.find_all('div', class_= class_key)
-    x = body[0].find_all('p')
+    content = soup.find_all('div', class_= class_key)
+    content = content[0].find_all('p')
 
+    #join all paragraphs together into one string and append to list of article text
     paragraphs = []
-    for p in np.arange(0, len(x)) :
+    for p in np.arange(0, len(content)) :
         paragraph = x[p].get_text()
         paragraphs.append(paragraph)
-        final_article = " ".join(paragraphs)
-        
-    text_list.append(final_article)
+        full_article = " ".join(paragraphs)
+    text_list.append(full_article)
 
+#join list of titles and list of text into one pandas dataframe, then code with binary indicators
 scraped_articles = pd.DataFrame(list(zip(title_list, text_list)),
                                columns =['title', 'text'])
 scraped_articles['fake'] = 1
@@ -89,10 +89,5 @@ scraped_articles['covid'] = 1
 
 print(scraped_articles)
 
-
-# In[140]:
-
-
-time = soup.find('time')
-print(time)
+#in the next update, we'll scrape true covid articles and join our datasets into a complete training set for analysis.
 
